87e800725c2707eeb854b518128d11be3779ef79
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-03-01 19:59:26 -0300

    using middleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -6,6 +7,21 @@ const app = express();
 app.use(express.json());
 
 const customers = [];
+
+// Middleware
+function verifyIfExistAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
 
 app.post("/account", (req, res) => {
   const { cpf, name  } = req.body;
@@ -28,15 +44,9 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
 });
 
-app.get("/statement", (req, res) => {
-  const { cpf } = req.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return res.status(400).json({ error: "Usuário não existe" });
-  }
-
+app.get("/statement", verifyIfExistAccountCPF, (req, res) => {
+  const { customer } = req;
+  
   return res.json(customer.statement);
 });
 
