187926eb488e7c2df0016632e1b10ce8fea8e3ce
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-03-01 22:08:21 -0300

    statement by date

--- src/index.js
+++ src/index.js
@@ -62,6 +62,21 @@ app.get("/statement", verifyIfExistAccountCPF, (req, res) => {
   return res.json(customer.statement);
 });
 
+app.get("/statement/date", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) => 
+      statement.created_at.toDateString() === 
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(customer.statement);
+});
+
 app.post("/deposit", verifyIfExistAccountCPF, (req, res) => {
   const { description, amount } = req.body;
 
